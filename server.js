b5e1f041b04f93d90636c49636908df35c59f398
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-03-19 02:27:15 +0100

    make it better

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 require("dotenv").config({ path: "./config.env" });
 
 const express = require("express");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
 const path = require("path");
+
 //Conect DB
-const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: true,
-  });
-  console.log("MongoDB connected");
-};
+connectDB();
 connectDB();
 const app = express();
 
