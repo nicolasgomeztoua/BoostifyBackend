f4f7f148d9d89d8223e82d58358c79c84eddd9c7
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-26 13:40:34 +0200

    username

--- controllers/private.js
+++ controllers/private.js
@@ -1,7 +1,13 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+
 exports.getPrivateData = (req, res, next) => {
+     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
   res.status(200).json({
     sucess: true,
     data: "You got acess to the private data in this route",
-    username: "test username",
+    username: user.username,
   });
 };
