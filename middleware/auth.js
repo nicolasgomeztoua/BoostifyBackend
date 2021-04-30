6eaf1a5a304b737f3ef1f7a59afe359d18769a71
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-30 23:47:18 +0200

    orderTracking

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const ErrorResponse = require("../utils/errorResponse");
-
+const Order = require("../models/Order");
 exports.protect = async (req, res, next) => {
   let token;
   if (
@@ -25,11 +25,13 @@ exports.protect = async (req, res, next) => {
     }
 
     req.user = user;
+    const orders = await Order.find({ userId: user.user_id });
     res.status(200).json({
       sucess: true,
       data: "You got acess to the private data in this route",
       username: user.username,
       user_id: user.user_id,
+      orders: orders,
     });
     next();
   } catch (error) {
