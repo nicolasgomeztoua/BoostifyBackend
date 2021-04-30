e75c7c2b499c6fe7c1f0326b495749e8896767dc
nicolasgomeztoua <nicolasnourgt@gmail.com>
2021-04-30 13:39:09 +0200

    profile

--- models/Order.js
+++ models/Order.js
@@ -18,7 +18,7 @@ const OrderSchema = new mongoose.Schema({
   platform: String,
   badgesExtras: [String],
   rankedImg: [String],
-  user_id: { type: [String], default: "" },
+  user_id: { type: [String] },
 });
 const Order = mongoose.model("Order", OrderSchema);
 
