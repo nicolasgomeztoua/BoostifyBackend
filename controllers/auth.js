e6370d543bab392a0377a0ad80198ea2def987ae
Nicolas Gomez <nicolas.gomez@mondia.com>
2021-09-13 17:01:39 +0200

    assummen

--- controllers/auth.js
+++ controllers/auth.js
@@ -1010,11 +1010,11 @@ exports.missingOrder = async (req, res, next) => {
       { orderId: orderId },
       { userId: userId },
       { new: true },
-      (error, data) => {
+      (error, reviews) => {
         if (error) {
           res.status(500).json({ err: error });
         } else {
-          res.status(200).json({ data: data });
+          res.status(200).json({ reviews: reviews });
         }
       }
     );
