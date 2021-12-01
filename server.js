1537329fa9e31b58adf17a2073fc50114da789e6
Nicolas Gomez <nicolas.gomez@mondia.com>
2021-12-01 14:28:14 +0100

    [MADCAMP-2601] fixing success redirect

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post("/create-checkout-session", async (req, res) => {
 
     success_url: `${"https://boostify.es/success"}?hash=${returnSuccessToken()}&gclid=${orderDetails.gclid}`,
     customer_email: items.email,
-    cancel_url: `${"https://boostify.es/cart"}?gclid=${orderDetails.gclid}&hash=${returnSuccessToken()}`,
+    cancel_url: `${"https://boostify.es/cart"}?gclid=${orderDetails.gclid}`,
     metadata: orderDetails,
   });
 
