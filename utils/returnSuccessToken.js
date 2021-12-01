0c20f0dd47535eeaf57fe06b50cbdf816fae4def
Nicolas Gomez <nicolas.gomez@mondia.com>
2021-12-01 03:06:48 +0100

    [MADCAMP-2601] fixing success redirect

--- utils/returnSuccessToken.js
+++ utils/returnSuccessToken.js
@@ -1,7 +1,7 @@
 
 const jwt = require("jsonwebtoken");
 exports.returnSuccessToken = () => {
-    return jwt.sign({ sucess: true }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE,
+    return jwt.sign({ sucess: true }, "hashSecret", {
+        expiresIn: "20m",
       });
 }
